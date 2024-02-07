58de04b8503eaa5f5af8f5bbdc13e3b31632097c
새미 <s22058@gsm.hs.kr>
2024-02-07 18:18:49 +0900

    🔧  :: Add SettingVC

--- ME/Controllers/TabBar/TabBarViewController.swift
+++ ME/Controllers/TabBar/TabBarViewController.swift
@@ -14,20 +14,25 @@ final class TabBarViewController: UITabBarController {
         
         let mainVC = MainViewController()
         let shopVC = ShopViewController()
+        let settingVC = SettingViewController()
     
         mainVC.title = "ME"
         shopVC.title = "Shop"
+        settingVC.title = "설정"
         
         mainVC.tabBarItem.image = UIImage.init(systemName: "house")
         shopVC.tabBarItem.image = UIImage.init(systemName: "bag")
+        settingVC.tabBarItem.image = UIImage.init(systemName: "person.fill")
         
         let navigationMain = UINavigationController(rootViewController: mainVC)
         let navigationShop = UINavigationController(rootViewController: shopVC)
+        let navigationSetting = UINavigationController(rootViewController: settingVC)
         
         navigationMain.navigationBar.prefersLargeTitles = true
         navigationShop.navigationBar.prefersLargeTitles = true
+        navigationSetting.navigationBar.prefersLargeTitles = true
         
-        setViewControllers([navigationMain, navigationShop], animated: false)
+        setViewControllers([navigationMain, navigationShop, navigationSetting], animated: false)
     }
     
     override func viewDidLayoutSubviews() {
