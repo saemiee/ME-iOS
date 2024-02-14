a495bee8155d4153c9b6850e26bcd76c74f12cec
새미 <s22058@gsm.hs.kr>
2024-02-14 16:12:16 +0900

    💄 :: Add ActivityRingView

--- ME/Controllers/Features/Main/MainViewController.swift
+++ ME/Controllers/Features/Main/MainViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 final class MainViewController: BaseViewController {
+    
+    // MARK: - Properties
+    private let myLabel = UILabel().then {
+        $0.text = "MY"
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 22, weight: .bold)
+    }
+    
+    private let workoutLabel = UILabel().then {
+        $0.text = "운동"
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 22, weight: .bold)
+    }
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
