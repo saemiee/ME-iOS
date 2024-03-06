74c21b758192ba80ee4126583d95b4d5ce49373d
새미 <s22058@gsm.hs.kr>
2024-03-06 23:34:10 +0900

    💄 :: Add RedeemSuccessPopup

--- ME/View/Button/PopupButton.swift
+++ ME/View/Button/PopupButton.swift
@@ -2,7 +2,7 @@
 //  PopupButton.swift
 //  ME
 //
-//  Created by 새미 on 2/8/24.
+//  Created by 새미 on 3/6/24.
 //
 
 import UIKit
@@ -12,6 +12,7 @@ final class PopupButton: UIButton {
     init(frame: CGRect, title: String) {
         super.init(frame: frame)
         self.frame = frame
+        
         setButton(withTitle: title)
     }
     
@@ -22,14 +23,14 @@ final class PopupButton: UIButton {
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        self.addBorder(positions: [.top], color: .white, width: 0.2)
+        self.addBorder(positions: [.top], color: .meLightGray, width: 1)
     }
     
     // MARK: - Button Setting
     func setButton(withTitle title: String) {
         backgroundColor = .clear
         setTitle(title, for: .normal)
-        setTitleColor(.meYellow, for: .normal)
+        setTitleColor(UIColor.yellow, for: .normal)
         titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .medium)
 
         self.roundCorners(cornerRadius: 10, maskedCorners: [.layerMinXMaxYCorner, .layerMaxXMaxYCorner])
