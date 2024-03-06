2327f3a9d212176d9a82d1462eea7348294b21e4
새미 <s22058@gsm.hs.kr>
2024-03-06 20:12:35 +0900

    💄 :: Add UseButton

---
+++ ME/View/Button/UseButton.swift
@@ -0,0 +1,30 @@
+//
+//  UseButton.swift
+//  ME
+//
+//  Created by 새미 on 3/6/24.
+//
+
+import UIKit
+
+final class UseButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setButton()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setButton() {
+        backgroundColor = .meOrange
+        setTitle("사용하기", for: .normal)
+        setTitleColor(UIColor.black, for: .normal)
+        titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        
+        layer.masksToBounds = true
+        layer.cornerRadius = 20
+    }
+}
