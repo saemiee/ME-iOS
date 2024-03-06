a35553e4e3c1a45fb26c75ac49459c67e266cd8c
새미 <s22058@gsm.hs.kr>
2024-03-06 20:20:09 +0900

    💄 :: Add static CouponCell Layout

--- ME/View/Cell/CouponCollectionViewCell.swift
+++ ME/View/Cell/CouponCollectionViewCell.swift
@@ -12,6 +12,8 @@ import Then
 
 final class CouponCollectionViewCell: UICollectionViewCell {
     // MARK: - Properties
+    static let identifier = "CouponCell"
+    
     let productImage = UIImageView()
     
     let brandName = UILabel().then {
@@ -31,11 +33,12 @@ final class CouponCollectionViewCell: UICollectionViewCell {
         $0.setDynamicTextColor(darkModeColor: .meLightGray, lightModeColor: .gray)
     }
     
+    private let useButton = UseButton()
+    
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
         addView()
-        setLayout()
     }
     
     required init?(coder: NSCoder) {
@@ -44,14 +47,38 @@ final class CouponCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Add View
     private func addView() {
+        [productImage, brandName, productLabel, expiryDateLabel, useButton].forEach { contentView.addSubview($0) }
     }
     
     // MARK: - Layout
     override func layoutSubviews() {
+        self.setDynamicBackgroundColor(darkModeColor: .meDarkGray, lightModeColor: .white)
+        self.layer.cornerRadius = 7
+        self.clipsToBounds = true
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.meGray.cgColor
         
-    }
-    
-    private func setLayout() {
+        productImage.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(22)
+            $0.centerY.equalToSuperview()
+        }
         
+        brandName.snp.makeConstraints {
+            $0.leading.equalTo(productImage.snp.trailing).offset(14)
+            $0.top.equalToSuperview().inset(15)
+            $0.height.equalTo(14)
+        }
+        
+        productLabel.snp.makeConstraints {
+            $0.leading.equalTo(productImage.snp.trailing).offset(14)
+            $0.top.equalTo(brandName.snp.bottom).offset(5)
+            $0.height.equalTo(19)
+        }
+        
+        expiryDateLabel.snp.makeConstraints {
+            $0.leading.equalTo(productImage.snp.trailing).offset(14)
+            $0.bottom.equalToSuperview().inset(14)
+            $0.height.equalTo(14)
+        }
     }
 }
