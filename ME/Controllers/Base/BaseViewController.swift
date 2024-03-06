2390d58febbf2f9ecdbc117dc422d7f4d07a626c
새미 <s22058@gsm.hs.kr>
2024-03-06 19:38:26 +0900

    💄 :: Add Border

--- ME/Controllers/Base/BaseViewController.swift
+++ ME/Controllers/Base/BaseViewController.swift
@@ -27,6 +27,7 @@ class BaseViewController: UIViewController {
         appearance.backgroundColor = .clear
         appearance.shadowColor = .clear
         
+        navigationController?.setNavigationBarHidden(false, animated: true)
         navigationController?.navigationBar.tintColor = .meOrange
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationController?.navigationBar.standardAppearance = appearance
