8554da39c441320b1018e3aa1e0fed6f6b783a95
새미 <s22058@gsm.hs.kr>
2024-03-07 20:10:46 +0900

    ✨ :: HealthKit get Data

--- ME/Service/HealthKitService.swift
+++ ME/Service/HealthKitService.swift
@@ -2,64 +2,42 @@
 //  HealthKitService.swift
 //  ME
 //
-//  Created by 새미 on 3/5/24.
+//  Created by 새미 on 3/7/24.
 //
 
 import HealthKit
 
 class HealthKitService {
+    
     let healthStore = HKHealthStore()
+    let share = Set([HKObjectType.workoutType()])
+    let read = Set([HKObjectType.workoutType()])
     
     let workoutTypes: [HKWorkoutActivityType] = [.walking, .swimming, .cycling, .pilates, .running, .elliptical, .coreTraining, .stepTraining, .functionalStrengthTraining, .hiking, .highIntensityIntervalTraining, .rowing]
     
-    let read = Set([HKWorkoutType.workoutType()])
-    let share = Set([HKWorkoutType.workoutType()])
-    
-    var caloriesByActivityType: [HKWorkoutActivityType: Double] = [:]
-    
-    func configure() {
-        if HKHealthStore.isHealthDataAvailable() {
-            requestAuthorization()
-        }
-    }
-    
-    private func requestAuthorization() {
-        self.healthStore.requestAuthorization(toShare: share, read: read) { success, error in
+    func requestAuthorization() {
+        self.healthStore.requestAuthorization(toShare: share, read: read) { scuccess, error in
             if error != nil {
-                print(error?.localizedDescription ?? "error")
+                print(error.debugDescription)
             } else {
-                if success {
-                    print("권한이 허락되었습니다.")
+                if scuccess {
+                    print("권한 허락")
                 } else {
-                    print("권한이 없습니다.")
+                    print("권한 없음")
                 }
             }
         }
     }
     
-    func getWorkoutData(completion: @escaping ([HKWorkoutActivityType: Double]?, Error?) -> Void) {
+    func getWorkoutData(completion: @escaping (Double) -> Void) {
         let calendar = Calendar.current
         let now = Date()
         let startOfDay = calendar.startOfDay(for: now)
-        let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+        let endOfDay = calendar.date(byAdding: .hour, value: 24, to: startOfDay)!
         let predicate = HKQuery.predicateForSamples(withStart: startOfDay, end: endOfDay, options: .strictEndDate)
         
-        for activityType in workoutTypes {
-            let query = HKSampleQuery(sampleType: HKObjectType.workoutType(), predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { query, samples, error in
-                guard let workouts = samples as? [HKWorkout], error == nil else {
-                    completion(nil, error)
-                    return
-                }
-                
-                let totalCalories = workouts.reduce(0.0) { result, workout in
-                    return result + (workout.totalEnergyBurned?.doubleValue(for: HKUnit.kilocalorie()) ?? 0.0)
-                }
-                
-                self.caloriesByActivityType[activityType] = totalCalories
-                completion(self.caloriesByActivityType, nil)
-            }
-            
-            self.healthStore.execute(query)
+        let query = HKSampleQuery(sampleType: HKObjectType.workoutType(), predicate: predicate, limit: HKObjectQueryNoLimit, sortDescriptors: nil) { query, result, error in
+            // 각 소모 에너지 전달
         }
     }
 }
